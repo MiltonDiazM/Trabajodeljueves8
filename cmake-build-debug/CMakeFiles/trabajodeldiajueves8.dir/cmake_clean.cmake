9bf5df7574951d7649c2e13bb413c1f549771cc2
Milton Diaz <diazmorcillomilton@gmail.com>
2021-07-08 15:58:45 -0500

    trabajo del jueves

---
+++ cmake-build-debug/CMakeFiles/trabajodeldiajueves8.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/trabajodeldiajueves8.dir/main.cpp.obj"
+  "libtrabajodeldiajueves8.dll.a"
+  "trabajodeldiajueves8.exe"
+  "trabajodeldiajueves8.exe.manifest"
+  "trabajodeldiajueves8.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/trabajodeldiajueves8.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
